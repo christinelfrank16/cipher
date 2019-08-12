bcea2b86d2cf932875a45a73526fcbcca3a81968
Christine Frank and Jacqueline Remmel <jacqueline@>
2019-08-12 14:35:02 -0700

    Add comments to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
+// This script alters the user's input and
+// displays the  original or ciphered value
+// when the user clicks on the respective img
 
+
+// initialize after page is ready
 jQuery(document).ready(function() {
-  var sentenceInput = prompt("Enter a sentence below:");
-  console.log(sentenceInput);
 
+  // prompt user for sentence to cipher
+  var sentenceInput = prompt("Enter a sentence below:");
+  // FOR DEBUGGING: console.log(sentenceInput);
+
+
+  // Returns uppercase first and last letter of input string
   var firstLastLetter = function(sentence){
     return (sentence[0]+sentence[sentence.length-1]).toUpperCase();
   }
-  console.log(firstLastLetter(sentenceInput));
+  // FOR DEBUGGING: console.log(firstLastLetter(sentenceInput));
 
-  var newSentence = firstLastLetter(sentenceInput);
 
+  // Returns the input string in reverse order
   var reverseOrder = function(sentence) {
     var newSentence = "";
     for(i = sentence.length-1; i >= 0; i--){
@@ -17,24 +26,30 @@ jQuery(document).ready(function() {
     }
     return newSentence;
   }
-  console.log(reverseOrder(sentenceInput));
+  // FOR DEBUGGING: console.log(reverseOrder(sentenceInput));
 
+
+  // Finds and returns the middle letter of the input string
   var middleLetter = function(sentence){
     return sentence[Math.floor(sentence.length/2)];
   }
-  console.log(middleLetter(sentenceInput));
+  // FOR DEBUGGING: console.log(middleLetter(sentenceInput));
 
+
+  // Ciphers the input string
   var combineFunctions = function(sentence) {
     var modifiedSentence = middleLetter(sentence) + sentence + reverseOrder(firstLastLetter(sentence));
     return  reverseOrder(modifiedSentence);
   }
+  // FOR DEBUGGING: console.log(combineFunctions(sentenceInput));
 
-  console.log(combineFunctions(sentenceInput));
 
+  // Event listener to display the ciphered string
   $("#locked").click(function() {
     document.getElementById("sentence").innerHTML = combineFunctions(sentenceInput);
   });
 
+  // Event listener to display the original string
   $("#unlocked").click(function () {
     document.getElementById("sentence").innerHTML = sentenceInput;
   });
