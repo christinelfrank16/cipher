b11ffc00814283e1e185e179d5e2d55f4fb06a7b
Christine Frank and Jacqueline Remmel <jacqueline@>
2019-08-12 14:21:15 -0700

    Write JS script to cipher input text. Add locked and unlocked images in HTML. Add click listener to locked image to show ciphered text on click

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+jQuery(document).ready(function() {
+  var sentenceInput = prompt("Enter a sentence below:");
+  console.log(sentenceInput);
+
+  var firstLastLetter = function(sentence){
+    return (sentence[0]+sentence[sentence.length-1]).toUpperCase();
+  }
+  console.log(firstLastLetter(sentenceInput));
+
+  var newSentence = firstLastLetter(sentenceInput);
+
+  var reverseOrder = function(sentence) {
+    var newSentence = "";
+    for(i = sentence.length-1; i >= 0; i--){
+      newSentence += sentence[i];
+    }
+    return newSentence;
+  }
+  console.log(reverseOrder(sentenceInput));
+
+  var middleLetter = function(sentence){
+    return sentence[Math.floor(sentence.length/2)];
+  }
+  console.log(middleLetter(sentenceInput));
+
+  var combineFunctions = function(sentence) {
+    var modifiedSentence = middleLetter(sentence) + sentence + reverseOrder(firstLastLetter(sentence));
+    return  reverseOrder(modifiedSentence);
+  }
+
+  console.log(combineFunctions(sentenceInput));
+
+  $("#locked").click(function() {
+    document.getElementById("sentence").innerHTML = combineFunctions(sentenceInput);
+
+    // return combineFunctions(sentenceInput);
+  });
+
+});
